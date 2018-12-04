3a28164cafa1f61ab50b02317dcd3185ecb9b0c0
Nikita <fobos3100@gmail.com>
2018-12-04 23:10:37 +0300

    aaa

---
+++ WHITEandBLACK/Assets/THINGS/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    Player player;
+    public GameObject anotherDoorPoint;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetButtonDown("Use"))
+        {
+            player.transform.position = anotherDoorPoint.transform.position;
+        }
+    }
+}
---
+++ WHITEandBLACK/Assets/THINGS/Scripts/FallingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public float fallDelay;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.isKinematic = false;
+        yield return 0;
+    }
+}
