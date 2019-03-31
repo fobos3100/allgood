12d6137714274d149a6015d75795d1a1c7028533
Nikita <fobos3100@gmail.com>
2019-03-31 03:36:10 +0300

    a

---
+++ WHITEandBLACK/Assets/THINGS/Scripts/TestScript/Controller2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Controller2D))]
+
+public class PlayerTest : MonoBehaviour {
+
+    Controller2D controller;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
